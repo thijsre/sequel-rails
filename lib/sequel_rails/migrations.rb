fee3991d5bf76051f4513a18c3422a7ad4bf2980
Thijs <thijs@easygis.eu>
2017-11-30 23:00:08 +0100

    Progress

--- lib/sequel_rails/migrations.rb
+++ lib/sequel_rails/migrations.rb
@@ -61,6 +61,51 @@ module SequelRails
       def available_migrations?
         File.exist?(migrations_dir) && Dir[File.join(migrations_dir, '*')].any?
       end
+
+      def copy(destination, sources, options = {})
+        copied = []
+
+        FileUtils.mkdir_p(destination) unless File.exist?(destination)
+
+        destination_migrations = ActiveRecord::Migrator.migrations(destination)
+        last = destination_migrations.last
+        sources.each do |scope, path|
+          source_migrations = ActiveRecord::Migrator.migrations(path)
+
+          source_migrations.each do |migration|
+            source = File.binread(migration.filename)
+            inserted_comment = "# This migration comes from #{scope} (originally #{migration.version})\n"
+            if /\A#.*\b(?:en)?coding:\s*\S+/ =~ source
+              # If we have a magic comment in the original migration,
+              # insert our comment after the first newline(end of the magic comment line)
+              # so the magic keep working.
+              # Note that magic comments must be at the first line(except sh-bang).
+              source[/\n/] = "\n#{inserted_comment}"
+            else
+              source = "#{inserted_comment}#{source}"
+            end
+
+            if duplicate = destination_migrations.detect { |m| m.name == migration.name }
+              if options[:on_skip] && duplicate.scope != scope.to_s
+                options[:on_skip].call(scope, migration)
+              end
+              next
+            end
+
+            migration.version = next_migration_number(last ? last.version + 1 : 0).to_i
+            new_path = File.join(destination, "#{migration.version}_#{migration.name.underscore}.#{scope}.rb")
+            old_path, migration.filename = migration.filename, new_path
+            last = migration
+
+            File.binwrite(migration.filename, source)
+            copied << migration
+            options[:on_copy].call(scope, migration, old_path) if options[:on_copy]
+            destination_migrations << migration
+          end
+        end
+
+        copied
+      end
     end
   end
 end
--- lib/sequel_rails/railties/database.rake
+++ lib/sequel_rails/railties/database.rake
@@ -208,3 +208,31 @@ namespace sequel_rails_namespace do
 end
 
 task 'test:prepare' => "#{sequel_rails_namespace}:test:prepare"
+
+namespace :railties do
+  namespace :install do
+    # desc "Copies missing migrations from Railties (e.g. engines). You can specify Railties to use with FROM=railtie1,railtie2"
+    task :migrations do
+      to_load = ENV["FROM"].blank? ? :all : ENV["FROM"].split(",").map(&:strip)
+      railties = {}
+      Rails.application.migration_railties.each do |railtie|
+        next unless to_load == :all || to_load.include?(railtie.railtie_name)
+
+        if railtie.respond_to?(:paths) && (path = railtie.paths["db/migrate"].first)
+          railties[railtie.railtie_name] = path
+        end
+      end
+
+      on_skip = Proc.new do |name, migration|
+        puts "NOTE: Migration #{migration.basename} from #{name} has been skipped. Migration with the same name already exists."
+      end
+
+      on_copy = Proc.new do |name, migration|
+        puts "Copied migration #{migration.basename} from #{name}"
+      end
+
+      Sequel::Migrations.copy(SequelRails::Migrations.migrations_dir, railties,
+                              on_skip: on_skip, on_copy: on_copy)
+    end
+  end
+end
